5d0f08602342f1bff52f1cc9fd086a9ffd558265
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2020-08-14 00:29:11 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,28 @@
+// server
+const express = require('express')
+const server = express()
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+// configurar nunjucks (template engine)
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+// Inicio e configuração do servidor
+server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+// configurar arquvos estaticos (css, scripts, images)
+.use(express.static("public"))
+//
+// rotas de aplicação 
+.get("/", pageLanding)
+.get("/study",pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+// start do servidor
+.listen(5500)
+
